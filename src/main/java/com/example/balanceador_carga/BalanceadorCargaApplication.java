44e4cd6973107d747f5ecbbd4ff438a191715f94
Melisa <melissalantigua@hotmail.com>
2018-10-12 19:56:15 -0400

    Commit inicial

---
+++ src/main/java/com/example/balanceador_carga/BalanceadorCargaApplication.java
@@ -0,0 +1,12 @@
+package com.example.balanceador_carga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BalanceadorCargaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BalanceadorCargaApplication.class, args);
+    }
+}
